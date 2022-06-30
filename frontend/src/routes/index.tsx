8b4fdc67f92373f086d8ec62bd6e1b757d6e7b78
Luís Melo <luis.f.duraes.melo@gmail.com>
2022-06-30 19:11:57 +0100

    Update index.tsx

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,8 +3,8 @@ import { LandingPage } from '../pages/LandingPage/LandingPage';
 import { MyPage } from '../pages/MyPage/MyPage';
 import { SignPage } from '../pages/SignPage/SignPage';
 
+const log = localStorage.getItem('isLogged');
 export const AppRoutes = () => {
-  const log = localStorage.getItem('isLogged');
   let logged;
   if (log === '1') logged = true;
   if (log !== '1') logged = false;
