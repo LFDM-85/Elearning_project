4c9e5e4f53392acedfaefa709ec34c0c8cc03008
Luís Melo <luis.f.duraes.melo@gmail.com>
2022-07-22 12:26:44 +0100

    security: Remove CSRF Protection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.use(csurf());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
