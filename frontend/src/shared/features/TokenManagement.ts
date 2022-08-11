da7055ecf3df0a46e95ce45a2e2192c7dcc3449f
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-08-11 10:44:41 +0100

    update

--- frontend/src/shared/features/TokenManagement.ts
+++ frontend/src/shared/features/TokenManagement.ts
@@ -3,25 +3,11 @@ import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
 export function getToken() {
-  return localStorage.getItem('token');
+  // return localStorage.getItem('token');
+  return cookies.get('token');
 }
 
 export function setToken(token: string) {
-  return localStorage.setItem('token', token);
-}
-
-export function getRefreshToken() {
-  return cookies.get('refreshCookie');
-
-  // return sessionStorage.getItem('refreshToken');
-}
-
-export function setRefreshToken(refreshToken: string) {
-  // return cookies.set('refreshCookie', refreshToken, {
-  //   httpOnly: true,
-  //   sameSite: 'none',
-  //   maxAge: 48 * 60 * 60 * 1000,
-  // });
-
-  return sessionStorage.setItem('refreshToken', refreshToken);
+  // return localStorage.setItem('token', token);
+  return cookies.set('token', token);
 }
