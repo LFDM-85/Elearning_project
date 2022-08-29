0f8d08bf76db4d3beb0226823c65e045486f2f5c
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-08-29 14:07:47 +0100

    feature: vercel

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as dotenv from 'dotenv';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 dotenv.config();
 
@@ -17,6 +19,9 @@ const URL = process.env.DATABASE_URL;
     ThrottlerModule.forRoot({
       ttl: 60,
       limit: 10,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
     }),
   ],
   controllers: [],
