6f99c3de5285181ef374c49197cd2e5dabf6770c
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-06 17:53:13 +0100

    feature: update, findAll and delete users

--- backend/dist/users/schema/users.schema.d.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-export declare const UsersSchema: mongoose.Schema<any, mongoose.Model<any, any, any, any, any>, {}, {}, {}, {}, "type", {
-    name: string;
-    email: string;
-    password: string;
-    role: any[];
-    refreshToken?: string;
-    refreshTokenExp?: string;
-}>;
